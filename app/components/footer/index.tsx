7edbcb473067696f20f57bcca4b378fed9eab884
Diogo <89143867+Diogo1006br@users.noreply.github.com>
2025-09-12 11:29:16 -0300

    Finished de front-end part

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -4,9 +4,9 @@ export const Footer = () => {
     return (
         <footer className="h-14 w-full flex items-center justify-center bg-gray-950">
             <span className='flex items-center gap-1.5 text-xs sm:text-sm font-mono text-gray-400'>
-                Made
+                Feito com
                 <IoMdHeart className="text-emerald-500"/>
-                by
+                por
                 <strong className="font-medium">Diogo Ramuski</strong>
             </span>
         </footer>
--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,9 +1,11 @@
 import { ProjectDetails } from "@/app/components/pages/home/project/project-details";
+import { ProjectSections } from "@/app/components/pages/home/project/project-sections";
 
 export default function Project() {
     return (
         <>
         <ProjectDetails/>
+        <ProjectSections/>
         </>
     )
 }
