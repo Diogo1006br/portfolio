ef9e2b962ec54bb9cda552f27d8e3da3763efd60
Diogo <89143867+Diogo1006br@users.noreply.github.com>
2025-09-24 23:33:17 -0300

    add new motion framer and global css

--- app/components/pages/home/project/project-sections/index.tsx
+++ app/components/pages/home/project/project-sections/index.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 import { ProjectSection } from '@/app/types/projects'
+import { fadeUpAnimation } from '@/app/lib/animations'
 
 type ProjectSectionsProps = {
   sections: ProjectSection[]
@@ -15,6 +16,8 @@ export const ProjectSections = ({ sections }: ProjectSectionsProps) => {
         <motion.div
           key={section.title}
           className="flex flex-col items-center gap-6 md:gap-12"
+          {...fadeUpAnimation}
+          transition={{ duration: 0.5 }}
         >
           <h2 className="text-2xl md:text-3xl font-medium text-gray-300">
             {section.title}
